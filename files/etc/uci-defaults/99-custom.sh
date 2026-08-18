c67bc08fb6317aecec8db5449789483c219cd05b
Axlyee42 <smashing2003@outlook.com>
2026-08-18 20:43:31 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,24 +1,17 @@
 #!/bin/sh
 #
-# OpenWrt x86-64 ImageBuilder first-boot customization.
-#
-# The workflow replaces __LAN_IP__ before building.
+# OpenWrt x86-64 ImageBuilder first-boot LAN configuration.
+# The GitHub Actions workflow replaces __LAN_IP__ before building.
 #
 
 LAN_IP="__LAN_IP__"
 
-case "${LAN_IP}" in
-  ""|*[!0-9.]*)
-    exit 1
-    ;;
-esac
+[ -n "${LAN_IP}" ] || exit 0
 
-uci -q batch <<EOF
-set network.lan.proto='static'
-set network.lan.ipaddr='${LAN_IP}'
-set network.lan.netmask='255.255.255.0'
-EOF
+uci -q set "network.lan.proto=static"
+uci -q set "network.lan.ipaddr=${LAN_IP}"
+uci -q set "network.lan.netmask=255.255.255.0"
 
-uci commit network
+uci -q commit network
 
 exit 0
