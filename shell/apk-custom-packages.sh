492a61e92d89a77182840c896a88836c8844e6a5
Axlyee42 <smashing2003@outlook.com>
2026-08-18 18:26:56 +0800

    Create apk-custom-packages.sh for OpenWrt packages
    Added a script for third-party APK packages for OpenWrt.

---
+++ shell/apk-custom-packages.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# ==========================================================
+# Third-party APK packages for OpenWrt 25.12.x
+# ==========================================================
+#
+# 这里只填写第三方 APK。
+#
+# OpenWrt 官方仓库中的软件不要放在这里。
+#
+# ==========================================================
+
+CUSTOM_PACKAGES="
+luci-theme-aurora
+luci-app-aurora-config
+luci-i18n-aurora-config-zh-cn
+
+bandix
+luci-app-bandix
+luci-i18n-bandix-zh-cn
+
+luci-app-run
+
+luci-app-filemanager
+luci-i18n-filemanager-zh-cn
+
+luci-app-filebrowser-go
+luci-i18n-filebrowser-go-zh-cn
+
+luci-app-timewol
+luci-i18n-timewol-zh-cn
+
+luci-app-vlmcsd
+luci-i18n-vlmcsd-zh-cn
+
+luci-app-autoreboot
+luci-i18n-autoreboot-zh-cn
+
+luci-app-passwall
+luci-i18n-passwall-zh-cn
+
+geoview
+xray-core
+sing-box
+hysteria
+
+luci-app-openclash
+"
