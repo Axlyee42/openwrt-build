20f6feaba8e20d7878115a67d0518f1e4216c802
Axlyee42 <smashing2003@outlook.com>
2026-08-18 20:23:35 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 #
 # OpenWrt x86-64 ImageBuilder first-boot customization.
+#
 # The workflow replaces __LAN_IP__ before building.
 #
 
 LAN_IP="__LAN_IP__"
+
+case "${LAN_IP}" in
+  ""|*[!0-9.]*)
+    exit 1
+    ;;
+esac
 
 uci -q batch <<EOF
 set network.lan.proto='static'
