cf2fa48083f3a1e4e2aeae38350d7cc5f1b12556
Axlyee42 <smashing2003@outlook.com>
2026-08-22 14:04:35 +0800

    feat: add aurora apk feed source for runtime updates

--- shell/apk-prepare-thirdparty-sources.sh
+++ shell/apk-prepare-thirdparty-sources.sh
@@ -7,19 +7,22 @@ KEY_DIR="$IMAGE_FILES_DIR/etc/apk/keys"
 
 mkdir -p "$APK_DIR" "$KEY_DIR"
 
+# PassWall APK feed
 cat > "$APK_DIR/passwall.list" <<'EOF'
 https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-25.12/x86_64/passwall_luci/packages.adb
 https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-25.12/x86_64/passwall_packages/packages.adb
 https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-25.12/x86_64/passwall2/packages.adb
 EOF
 
+# Aurora official APK feed
 cat > "$APK_DIR/aurora.list" <<'EOF'
-# luci-theme-aurora repository will be provided by the custom apk repository
+https://openwrt.eamonxg.fun/packages/packages.adb
 EOF
 
-# PassWall public key will be installed on first boot if required
+# PassWall public key installation on first boot
 cat > "$IMAGE_FILES_DIR/etc/uci-defaults/99-thirdparty-apk-sources" <<'EOF'
 #!/bin/sh
+
 mkdir -p /etc/apk/keys /etc/apk/repositories.d
 
 wget -O /etc/apk/keys/openwrt-passwall-build.pem \
