f375ff921fc5c2af1929f7bcbcefed1229abae4c
Axlyee42 <smashing2003@outlook.com>
2026-08-22 09:26:40 +0800

    refactor: restore wukongdaily package helper structure

---
+++ shell/custom-packages.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# ============= OpenWrt 仓库外的第三方插件 =============
+# ============= 若启用则打开注释 =======================
+
+# Run 安装器
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-run"
+# 极光主题
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-aurora luci-app-aurora-config luci-i18n-aurora-config-zh-cn"
+# AdGuardHome
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-adguardhome"
+# DAE / DAED
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-dae-zh-cn"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-daed-zh-cn"
+# PassWall
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES geoview xray-core sing-box hysteria luci-i18n-passwall-zh-cn"
+# PassWall2
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES geoview xray-core sing-box hysteria kmod-nft-socket kmod-nft-tproxy luci-app-passwall2 luci-i18n-passwall2-zh-cn"
+# OpenClash
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash luci-compat kmod-tun kmod-inet-diag kmod-nft-tproxy bash curl ip-full unzip"
+# SSR Plus
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-nft-tproxy kmod-nft-socket xray-core naiveproxy luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn"
+# Nikki
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-nikki-zh-cn"
+# WireGuard
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-proto-wireguard"
+# Tailscale
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale-community luci-i18n-tailscale-community-zh-cn"
+# 分区扩容
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp luci-i18n-partexp-zh-cn"
+# Watchdog
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-watchdog luci-i18n-watchdog-zh-cn"
+# MosDNS
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mosdns luci-i18n-mosdns-zh-cn"
+# Turbo ACC
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc"
+# Lucky
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-lucky lucky"
+# Bandix
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-bandix luci-i18n-bandix-zh-cn"
+# EasyTier
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES easytier luci-app-easytier"
+# WebDAV
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES webdav2 luci-app-unishare"
+# Dufs
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-dufs-zh-cn"
