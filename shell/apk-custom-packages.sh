28ff0379c7363b7a7c6b011060bb3d6c8640f7c3
Axlyee42 <smashing2003@outlook.com>
2026-09-05 11:27:24 +0800

    Update apk-custom-packages.sh

--- shell/apk-custom-packages.sh
+++ shell/apk-custom-packages.sh
@@ -14,22 +14,22 @@ CUSTOM_PACKAGES=""
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES geoview xray-core sing-box hysteria"
 
 # OpenClash + Chinese translation + required dependencies.
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash luci-i18n-openclash-zh-cn luci-compat kmod-tun kmod-inet-diag kmod-nft-tproxy bash curl ip-full unzip"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash luci-i18n-openclash-zh-cn luci-compat kmod-tun kmod-inet-diag kmod-nft-tproxy bash curl ip-full unzip"
 
 # PassWall + Chinese translation + proxy runtimes.
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-passwall luci-i18n-passwall-zh-cn geoview xray-core sing-box hysteria"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-passwall luci-i18n-passwall-zh-cn geoview xray-core sing-box hysteria"
 
 # HomeProxy + Chinese translation.
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-homeproxy luci-i18n-homeproxy-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-homeproxy luci-i18n-homeproxy-zh-cn"
 
 # MWAN3 + Chinese translation.
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mwan3 luci-i18n-mwan3-zh-cn"
 
 # Lucky + Chinese translation + runtime.
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-lucky luci-i18n-lucky-zh-cn lucky"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-lucky luci-i18n-lucky-zh-cn lucky"
 
 # DDNS-Go + Chinese translation + runtime.
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-ddns-go luci-i18n-ddns-go-zh-cn ddns-go"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-ddns-go luci-i18n-ddns-go-zh-cn ddns-go"
 
 CUSTOM_PACKAGES="$(printf '%s\n' "$CUSTOM_PACKAGES" | xargs)"
 export CUSTOM_PACKAGES
